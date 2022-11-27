356076edfe1f34971cc5cb7ca4792e62b00bf731
Jan <janswidzinskiprograming@gmail.com>
2022-11-27 22:35:12 +0100

    Finished integration of algorithm

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import pygame
-import threading
+import os
 from colors import *
 from node import create_random_nodes
-from ant import AntColony, start
+from ant import AntColony
 
 
 class App:
-    def __init__(self, weight=840, height=640, starting_node_number=5):
+    def __init__(self, weight=840, height=640, starting_node_number=4):
         self._running = False
         self._display_surf = None
         self.clock = None
@@ -28,7 +28,8 @@ class App:
         self.offset_x = 0
         self.offset_y = 0
 
-        self.iterations = 1000
+        self.iterations = 3
+        self.current_iteration = 1
         self.ants_multiplayer = 0.5
         self.evaporation_rate = 0.4
         self.random_movement_chance = 0.3
@@ -38,8 +39,12 @@ class App:
         self.best_route = None
         self.best_route_length = None
         self.ant_colony = None
-
         self.ant_colony_thread = None
+
+        self.ant_img = pygame.image.load(os.path.join("ant.png"))
+
+        self.simulate_ants = False
+        self.ants_not_moving = True
 
     def _pick_up_node(self, mouse_position):
         for node in self.node_list:
@@ -63,22 +68,20 @@ class App:
         print(f"Stopped Moving Node: {self.moving_node.id}")
         self.moving_node = None
 
-    def _get_best_path(self):
-        # or not self.ant_colony_thread.is_alive()
-        if self.ant_colony_thread is None:
-            print(f"Colony at work")
-            self.ant_colony_thread = threading.Thread(target=start,
-                                                      args=(self.ant_colony, self.node_distances,
-                                                            self.iterations, self), daemon=True)
-            self.ant_colony_thread.start()
-        else:
-            print("Kill")
-            self.ant_colony.stop_work = True
-            self.ant_colony_thread.join()
-            self.ant_colony_thread = None
-            self.ant_colony.stop_work = False
+    def _start_simulation(self):
+        print("Simulation started")
+        self.ant_colony = AntColony(self.node_number, self.ants_multiplayer, self.evaporation_rate,
+                                    self.random_movement_chance, self.alpha, self.beta)
 
+        self.ant_colony.create_ants(self.node_number, self.node_list)
 
+        self.simulate_ants = True
+        self.ants_not_moving = True
+        self.current_iteration = 1
+
+    def _stop_simulation(self):
+        print("Stopped simulation!")
+        self.simulate_ants = False
 
     def _draw_fps(self):
         text_surface = self.node_font.render("{:.2f}".format(self.clock.get_fps()), False, READ)
@@ -101,8 +104,15 @@ class App:
                 if start_node is end_node:
                     continue
                 else:
-                    start_node.draw_line(self._display_surf, end_node, self.node_distances, BLACK, self.line_thickness,
-                                         self.line_font, READ)
+                    start_node.draw_line(self._display_surf, end_node, self.node_distances,
+                                         BLACK, self.line_thickness, self.line_font, READ)
+
+    def _draw_ants(self):
+        self.ant_colony.draw_ants(self._display_surf, self.ant_img)
+
+    def _draw_iteration_counter(self):
+        text_surface = self.node_font.render(f"Iteration: {self.current_iteration}", False, READ)
+        self._display_surf.blit(text_surface, (200, 0))
 
     def on_init(self):
         pygame.init()
@@ -112,6 +122,8 @@ class App:
         self.slider_font = pygame.font.SysFont('Comic Sans MS', 18, bold=True)
 
         self._display_surf = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
+        self.ant_img.convert()
+
         self.clock = pygame.time.Clock()
         self._running = True
 
@@ -119,41 +131,59 @@ class App:
                                                                   self.node_font, BLACK,
                                                                   self.node_number)
 
-        self.ant_colony = AntColony(self.node_number, self.ants_multiplayer, self.evaporation_rate,
-                                    self.random_movement_chance, self.alpha, self.beta)
-
     def on_event(self, event):
         if event.type == pygame.QUIT:
             self._running = False
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if event.button == 1:
-                self._pick_up_node(event.pos)
+        if self.moving_node is None:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self._stop_simulation()
+                if event.key == pygame.K_s:
+                    self._start_simulation()
 
-        if event.type == pygame.MOUSEBUTTONUP:
-            if event.button == 1 and self.moving_node is not None:
-                self._drop_node()
+        if not self.simulate_ants:
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    self._pick_up_node(event.pos)
 
-        if event.type == pygame.MOUSEMOTION:
-            if self.moving_node is not None:
-                self._move_node(event.pos)
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1 and self.moving_node is not None:
+                    self._drop_node()
 
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:
-                self._get_best_path()
-            if event.key == pygame.K_s:
-                print(f"Sim")
+            if event.type == pygame.MOUSEMOTION:
+                if self.moving_node is not None:
+                    self._move_node(event.pos)
 
     def on_loop(self):
-        pass
+        if self.simulate_ants:
+            if self.ants_not_moving:
+                if self.iterations <= self.current_iteration:
+                    self.best_route, self.best_route_length = self.ant_colony.get_best_route(self.node_distances)
+                    self.simulate_ants = False
+                    self.current_iteration = 1
+                    self.ant_colony = AntColony(self.node_number, self.ants_multiplayer, self.evaporation_rate,
+                                                self.random_movement_chance, self.alpha, self.beta)
+
+                    self.ant_colony.create_ants(self.node_number, self.node_list)
+                else:
+                    self.current_iteration += 1
+                    self.ant_colony.iterate(self.node_distances, self.node_list)
+                    self.ants_not_moving = False
+            else:
+                self.ants_not_moving = self.ant_colony.move_ants_on_screen(self.node_list)
 
     def on_render(self):
-        print(threading.active_count())
         self._display_surf.fill(BACKGROUND_COLOR)
         self._draw_nodes_connections()
         if self.best_route is not None:
             self._draw_best_rout()
+
         self._draw_nodes()
+        if self.simulate_ants:
+            self._draw_ants()
+            self._draw_iteration_counter()
+
         self._draw_fps()
 
         pygame.display.flip()
